461a0bdd5472d7395b95923c1b5fc8437c7f1995
josesio <joseantoniodr07@gmail.com>
2025-11-26 07:57:37 -0400

    erase unused module

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const router = express.Router({mergeParams: true});
-const Review = require('../models/review.js');
 const catchAsync = require('../utils/catchAsync');
-const CampGround = require('../models/campgrounds');
 const {validateReview, isLoggedIn, isReviewAuthor} = require('../middleware.js');
 const reviews = require('../controllers/reviews.js');
 
